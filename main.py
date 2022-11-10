e3bc97038d8fcc532a88fef89bc04481a9960404
Ishmam Solaiman <ishmam.a.solaiman@gmail.com>
2022-11-10 11:24:26 -0600

    make youtube global variable

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from googleapiclient.discovery import build
 
 #gets channel statistics
 #uses channel id to retreive channel statistics as JSON/dict
-def get_channel_stats(youtube, channel_id):
+load_dotenv()
+yt_api_key = os.getenv('YOUTUBE_API_KEY')
+youtube = build('youtube', 'v3', developerKey=yt_api_key)
+
+def get_channel_stats(channel_id):
   request = youtube.channels().list(
     part='statistics',
     id=channel_id
@@ -16,7 +20,7 @@ def get_channel_stats(youtube, channel_id):
   return response
 
 #gets latest videos of channel
-def get_channel_activities(youtube, channel_id):
+def get_channel_activities(channel_id):
   request = youtube.activities().list(
     part='snippet,contentDetails',
     channelId=channel_id,
@@ -27,8 +31,8 @@ def get_channel_activities(youtube, channel_id):
   return response
 
 #get latest video ids of channels
-def get_recent_video_ids(youtube, channel_id):
-  activities = get_channel_activities(youtube, channel_id)
+def get_recent_video_ids(channel_id):
+  activities = get_channel_activities(channel_id)
   ids = []
   for activity in activities['items']:
     ids.append(activity['contentDetails']['upload']['videoId'])
@@ -36,7 +40,7 @@ def get_recent_video_ids(youtube, channel_id):
   return ids
 
 #given vid find related vids
-def get_related_videos(youtube, video_id):
+def get_related_videos(video_id):
   request = youtube.search().list(
     part="snippet",
     relatedToVideoId=video_id,
@@ -52,18 +56,13 @@ def get_related_videos(youtube, video_id):
 #function for saving video information as csv/to database
 
 def main():
-  load_dotenv()
-  yt_api_key = os.getenv('YOUTUBE_API_KEY')
   
   channel_id = "UCxeOc7eFxq37yW_Nc-69deA"
   video_id = "g7o2Rr1dsXQ"
-
-  youtube = build('youtube', 'v3', developerKey=yt_api_key)
-
-  # pprint(get_channel_activities(youtube, channel_id))
-  # pprint(get_channel_stats(youtube, channel_id))
-  # pprint(get_related_videos(youtube, video_id))
-  print(get_recent_video_ids(youtube, channel_id))
+  pprint(get_channel_activities(channel_id))
+  # pprint(get_channel_stats(channel_id))
+  # pprint(get_related_videos(video_id))
+  # print(get_recent_video_ids(channel_id))
 
 if __name__ == "__main__":
   main()
